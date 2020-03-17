ef70f3e2073c09e5dd6a2b2123c7ff8fb7173eed
Phil Sautter <philsautter@phils-mbp.attlocal.net>
2020-03-17 10:55:46 -0400

    very basic aws ec2 provisioning

---
+++ onboarding-demo.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = var.AWS_REGION
+}
+
+data "aws_ami" "amazon_linux_2" {
+    most_recent = true
+
+    filter {
+        name = "name"
+        values = ["amzn2-ami-hvm-*.*-x86_64-gp2"]
+    }
+
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+
+    owners = ["137112412989"]
+}
+
+resource "aws_instance" "terraform_dynamic_ami" {
+    count = var.INSTANCE_COUNT
+    ami = data.aws_ami.amazon_linux_2.id
+    instance_type = var.AWS_INSTANCE_TYPE
+
+    tags = {
+        name = var.TAG_USER_NAME
+    }
+}
